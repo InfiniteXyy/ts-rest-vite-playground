da883e7b24ce81b4798490f91e4dd2b22949a8cf
InfiniteXyy <xuyiyangwing@gmail.com>
2024-05-19 18:56:22 +0800

    fix id incremental bug

--- src/model.ts
+++ src/model.ts
@@ -1,15 +1,15 @@
-import { z } from "zod";
-import { PostSchema } from "./contract";
+import { z } from 'zod';
+import { PostSchema } from './contract';
 
 type Post = z.infer<typeof PostSchema>;
 
-const db: Post[] = [{ id: "0", body: "test", title: "hello" }];
+const db: Post[] = [{ id: '0', body: 'test', title: 'hello' }];
 
 let id = 0;
 
 export const dbManager = {
-  insert(post: Omit<Post, "id">) {
-    const item = { ...post, id: String(id++) };
+  insert(post: Omit<Post, 'id'>) {
+    const item = { ...post, id: String(++id) };
     db.push(item);
     return item;
   },
